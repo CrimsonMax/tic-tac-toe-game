7417e8f71561440db1fd9aa2c16ea50f1943266d
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2020-10-29 23:18:09 +0300

    Done

---
+++ site.js
@@ -0,0 +1,88 @@
+let gameField = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
+
+$('.row button').on('click', event => {
+    let x = event.target.dataset.btn;
+    let y = event.target.parentElement.dataset.row;
+
+    if (isExist(x, y)) {
+        return null;
+    }
+
+    gameField[x][y] = getCount() % 2 ? 'O' : 'X';
+
+    let sec1 = document.getElementById('0');
+    let sec2 = document.getElementById('1');
+    let sec3 = document.getElementById('2');
+    let sec4 = document.getElementById('3');
+    let sec5 = document.getElementById('4');
+    let sec6 = document.getElementById('5');
+    let sec7 = document.getElementById('6');
+    let sec8 = document.getElementById('7');
+    let sec9 = document.getElementById('8');
+
+    function finish() {
+        if (sec1.innerText == 'X' && sec2.innerText == 'X' && sec3.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec1.innerText == 'O' && sec2.innerText == 'O' && sec3.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec4.innerText == 'X' && sec5.innerText == 'X' && sec6.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec4.innerText == 'O' && sec5.innerText == 'O' && sec6.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec7.innerText == 'X' && sec8.innerText == 'X' && sec9.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec7.innerText == 'O' && sec8.innerText == 'O' && sec9.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec1.innerText == 'X' && sec4.innerText == 'X' && sec7.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec1.innerText == 'O' && sec4.innerText == 'O' && sec7.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec2.innerText == 'X' && sec5.innerText == 'X' && sec8.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec2.innerText == 'O' && sec5.innerText == 'O' && sec8.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec3.innerText == 'X' && sec6.innerText == 'X' && sec9.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec3.innerText == 'O' && sec6.innerText == 'O' && sec9.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec5.innerText == 'X' && sec1.innerText == 'X' && sec9.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec5.innerText == 'O' && sec1.innerText == 'O' && sec9.innerText == 'O') {
+            return alert('O WIN!');
+        }
+        if (sec5.innerText == 'X' && sec3.innerText == 'X' && sec7.innerText == 'X') {
+            return alert('X WIN!');
+        } else if (sec5.innerText == 'O' && sec3.innerText == 'O' && sec7.innerText == 'O') {
+            return alert('O WIN!');
+        }
+    }
+
+    $(`[data-row="${y}"] [data-btn="${x}"]`).text(gameField[x][y]);
+    finish();
+});
+
+function getCount() {
+    counter = 0;
+    gameField.forEach(row => {
+        row.forEach(btn => {
+            if (btn) {
+                counter++;
+            }
+        });
+    });
+    return counter;
+}
+
+function isExist(x, y) {
+    return gameField[x][y] !== null;
+}
